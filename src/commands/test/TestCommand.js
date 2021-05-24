deb0a9d425f3f2147edcdecbcb980772b34b0b11
YazidIlyasBaihaqi <yazi20012ti@student.nurulfikri.ac.id>
2021-05-24 09:50:24 +0700

    Update TestCommand.js

--- src/commands/test/TestCommand.js
+++ src/commands/test/TestCommand.js
@@ -17,34 +17,7 @@ module.exports = class TestCommand extends BaseCommand {
   }
 
   async run(client, message, args) {
-    const canvas = Canvas.createCanvas(1024, 500);
-    const context = canvas.getContext('2d');
-    const mentionedMember = message.mentions.members.first();
-    const background = await Canvas.loadImage('./src/img/wallpaper.png');
-    
-    context.drawImage(background, 0, 0, canvas.width, canvas.height); 
-    context.strokeStyle = '#74037b';
-    context.strokeRect(0, 0, canvas.width, canvas.height);
-    
-    const avatar = await Canvas.loadImage(mentionedMember.user.displayAvatarURL({ format: 'jpg'}));
-    context.font = '40px sans-serif';
-    context.fillStyle = '#ffffff';
-    context.textAlign = 'center';
-    context.fillText('Welcome to the server,', canvas.width / 2, 325);
-    
-    context.font = applyText(canvas, `${mentionedMember.displayname}!`);
-    context.fillStyle = '#ffffff';
-    context.textAlign = 'center';
-    context.fillText(`${mentionedMember.user.tag}!`, canvas.width / 2, 370);
-
-    ///
-    context.beginPath();
-    context.arc(canvas.width / 2.5 + 100, 184, 100, 0, Math.PI * 2, true);
-    context.closePath();
-    context.clip();
-    ///
-    context.drawImage(avatar, canvas.width / 2.5, 84, 200, 200);
-
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.jpg');
+    const welcomeChannel = member.guild.channels.cache.get('843377572448567333');
+    welcomeChannel.send('test diterima');
   }
 }
